419bdeb9cc4833fd4ad23276f5b92e52e54012e2
Haider Malik <haidermalik504@gmail.com>
2018-05-30 18:59:13 +0500

    add validations for request params

--- src/api/resources/song/song.controller.js
+++ src/api/resources/song/song.controller.js
@@ -1,5 +1,21 @@
+import Joi from 'joi';
+
 export default {
   async create(req, res) {
-    return res.json({ msg: 'TODO: SONG CREATE' });
+    console.log(req.body);
+    const schema = Joi.object().keys({
+      title: Joi.string().required(),
+      url: Joi.string().required(),
+      rating: Joi.number()
+        .integer()
+        .min(0)
+        .max(5)
+        .optional(),
+    });
+    const { value, error } = Joi.validate(req.body, schema);
+    if (error && error.details) {
+      return res.status(400).json(error);
+    }
+    return res.json(value);
   },
 };
--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const app = express();
 const PORT = 3000;
 
 connect();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(logger('dev'));
 
 app.use('/api', restRouter);
