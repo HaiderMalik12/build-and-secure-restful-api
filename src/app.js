ce580f35a4378c74f1433b42bb06b399f5673651
Haider Malik <haidermalik504@gmail.com>
2018-05-31 00:09:24 +0500

    setup swagger

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import logger from 'morgan';
+import swaggerUi from 'swagger-ui-express';
 import { connect } from './config/db';
 import { restRouter } from './api';
+import swaggerDocument from './config/swagger.json';
 
 const app = express();
 const PORT = 3000;
@@ -12,6 +14,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(logger('dev'));
 
 app.use('/api', restRouter);
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    explorer: true,
+  })
+);
 app.use((req, res, next) => {
   const error = new Error('Not found');
   error.message = 'Invalid route';
