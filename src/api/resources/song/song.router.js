6288e00042b8a620a0e369ee099ef75d2fed95fa
Haider Malik <haidermalik504@gmail.com>
2018-05-31 17:58:12 +0500

    apply authentication on routes

--- src/api/resources/song/song.router.js
+++ src/api/resources/song/song.router.js
@@ -1,14 +1,15 @@
 import express from 'express';
+import passport from 'passport';
 import songController from './song.controller';
 
 export const songRouter = express.Router();
 songRouter
   .route('/')
-  .post(songController.create)
-  .get(songController.findAll);
+  .post(passport.authenticate('jwt', { session: false }), songController.create)
+  .get(passport.authenticate('jwt', { session: false }), songController.findAll);
 
 songRouter
   .route('/:id')
-  .get(songController.findOne)
-  .delete(songController.delete)
-  .put(songController.update);
+  .get(passport.authenticate('jwt', { session: false }), songController.findOne)
+  .delete(passport.authenticate('jwt', { session: false }), songController.delete)
+  .put(passport.authenticate('jwt', { session: false }), songController.update);
