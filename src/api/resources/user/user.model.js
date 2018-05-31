d1a978753bd1d93c4131f3f771c37ef064a985b9
Haider Malik <haidermalik504@gmail.com>
2018-05-31 14:38:15 +0500

    create user model

---
+++ src/api/resources/user/user.model.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model('User', userSchema);
