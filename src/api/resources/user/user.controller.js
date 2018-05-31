f371c5bdbf70bc8bb1e10b6fbceabdb15d13c099
Haider Malik <haidermalik504@gmail.com>
2018-05-31 15:07:34 +0500

    user registration

--- src/api/resources/user/user.controller.js
+++ src/api/resources/user/user.controller.js
@@ -1,11 +1,25 @@
 import userService from './user.service';
+import User from './user.model';
 
 export default {
-  signup(req, res) {
-    const { value, error } = userService.validateSignup(req.body);
-    if (error) {
-      return res.status(400).json(error);
+  async signup(req, res) {
+    try {
+      const { value, error } = userService.validateSignup(req.body);
+      if (error) {
+        return res.status(400).json(error);
+      }
+      const encryptedPass = userService.encryptPassword(value.password);
+
+      const user = await User.create({
+        email: value.email,
+        firstName: value.firstName,
+        lastName: value.lastName,
+        password: encryptedPass,
+      });
+      return res.json({ success: true });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).send(err);
     }
-    return res.json(value);
   },
 };
--- src/api/resources/user/user.service.js
+++ src/api/resources/user/user.service.js
@@ -1,6 +1,11 @@
 import Joi from 'joi';
+import bcrypt from 'bcryptjs';
 
 export default {
+  encryptPassword(palinText) {
+    const salt = bcrypt.genSaltSync(10);
+    return bcrypt.hashSync(palinText, salt);
+  },
   validateSignup(body) {
     const schema = Joi.object().keys({
       firstName: Joi.string().required(),
